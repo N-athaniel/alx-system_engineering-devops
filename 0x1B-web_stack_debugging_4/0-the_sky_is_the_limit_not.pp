e2e5d840b22d2c9e1a142eb0105cd3f853a6414f
N-athaniel <ndoh1996@gmail.com>
2024-01-23 04:38:56 -0800

    Fixing the number of max open files per process

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+#  Increase limit of open files per user
+
+exec { 'fix--for-nginx':
+  command => "/bin/sed -i /etc/default/nginx -e 's/15/3000/'"
+}
+
+exec { 'restart nginx':
+  command => '/usr/sbin/service nginx restart',
+  require => Exec['fix--for-nginx']
+}
