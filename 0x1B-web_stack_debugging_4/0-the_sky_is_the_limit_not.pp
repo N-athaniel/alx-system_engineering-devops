0db6c863baac4215293670c0b101834fee377c0e
N-athaniel <ndoh1996@gmail.com>
2024-01-23 04:53:15 -0800

    fixing the stack

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,5 @@
 #  Increase limit of open files per user
+#  define an `exec` resource to fix the open files limit for Nginx
 
 exec { 'fix--for-nginx':
   command => "/bin/sed -i /etc/default/nginx -e 's/15/3000/'"
